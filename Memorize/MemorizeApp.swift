f57e209953cf335779da2749d15324d608c13c4d
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-07-31 09:48:59 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Luiz Henrique Silva Sampaio on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
