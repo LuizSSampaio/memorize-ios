34ccca7cf44e55f4708378c3e408898e3ae45bce
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-08-01 20:34:58 -0300

    📕 Lecture: Lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,17 +8,93 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var numberOfCards = 6
+    let emojis = [ "🛤", "🔴", "💺", "🈺", "♿️", "📢", "🐁", "🐻", "🌷", "💁", "6️⃣", "🚗", "🕟", "👈", "🚀", "📕" ]
+    
     var body: some View {
         VStack {
-            ZStack {
-                Text("🚀")
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 64.0))]) {
+                    ForEach(emojis[0..<numberOfCards], id: \.self, content: { emoji in
+                        CardView(content: emoji)
+                    })
+                }.foregroundColor(.orange)
+            }
+            
+            Spacer()
+            
+            HStack {
+                remove
+                
+                Spacer()
+                
+                shuffle
+                
+                Spacer()
+                
+                add
+            }
+            .font(.title)
+            .padding(.horizontal, 16.0)
+        }
+        .padding(.horizontal, 24.0)
+        .padding(.top, 24.0)
+    }
+    
+    var remove: some View {
+        Button {
+            if numberOfCards > 1 {
+                numberOfCards -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }.padding(8)
+    }
+    
+    var shuffle: some View {
+        Button("Shuffle") {
+            print("Shuffle")
+        }.padding(8)
+    }
+    
+    var add: some View {
+        Button {
+            if numberOfCards < emojis.count {
+                numberOfCards += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }.padding(8)
+    }
+        
+}
+
+struct CardView: View {
+    @State var isFaceUp: Bool = false
+    var content: String
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 8.0)
+            let shapeFilled = shape.fill()
+            
+            if isFaceUp {
+                shapeFilled
+                    .foregroundColor(.white)
              
-                RoundedRectangle(cornerRadius: 8.0)
-                    .stroke(lineWidth: 2.0)
-                    .foregroundColor(.orange)
+                shape
+                    .strokeBorder(lineWidth: 3.0)
+                
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                shapeFilled
             }
         }
-        .padding(24.0)
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+        .aspectRatio(2/3, contentMode: .fit)
     }
 }
 
